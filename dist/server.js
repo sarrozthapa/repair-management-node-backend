21fb4f4fa3c699d30fe5de48ff2c85cf7be3c825
Test Git <rahuljha07541@gmail.com>
2024-06-21 07:25:05 +0545

    hello

--- dist/server.js
+++ dist/server.js
@@ -31,19 +31,22 @@ dotenv_1.default.config({
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 exports.userSocketIDs = new Map();
-const corsOptions = {
+app.use((0, cors_1.default)({
     origin: [
-        "http://localhost:5173",
-        "http://localhost:4173",
         "http://localhost:3000",
+        "http://localhost:5174",
     ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-};
-app.use((0, cors_1.default)(corsOptions));
-const io = new socket_io_1.Server(server, corsOptions);
+}));
+const io = new socket_io_1.Server(server, {
+    cors: {
+        origin: [
+            "http://localhost:3000"
+        ],
+        credentials: true,
+    }
+});
 io.use((socket, next) => __awaiter(void 0, void 0, void 0, function* () {
-    console.log(socket);
     const socketAuthenticator = (err, socket, next) => __awaiter(void 0, void 0, void 0, function* () {
         try {
             if (err)
@@ -63,7 +66,7 @@ io.use((socket, next) => __awaiter(void 0, void 0, void 0, function* () {
 }));
 io.on('connection', (socket) => {
     console.log('connected', socket.id);
-    //userSocketIDs.set(socket.user.id,socket.id);
+    exports.userSocketIDs.set(socket.user.id, socket.id);
     socket.on('disconnect', () => {
         console.log('disconnected', socket.id);
         console.log('/n');
